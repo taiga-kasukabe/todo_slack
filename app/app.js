3be6d8b69a26cfc305be1c5e651450c6ff9fe29b
Shintaro Matsumoto <matsushin35@gmail.com>
2021-05-07 00:17:06 +0900

    add api & stylesheets dir

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const testDbRouter = require('./routes/test_output_db');
 const test1Router = require('./routes/test1');
+const apiRouter = require('./api/sampleapi')
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testdb', testDbRouter);
 app.use('/test1', test1Router);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
