5880abef78f1d4d28de66146f6e26cf04e82e9fa
Shintaro Matsumoto <matsushin35@gmail.com>
2021-05-06 23:57:20 +0900

    fix login.js

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,6 +1,6 @@
-// const passport = require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
-// const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const express = require('express');
 const router = express.Router();
 
