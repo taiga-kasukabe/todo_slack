a6e0f43b46e6d77f9bd2beaf2d0ad04f91cb2b90
Shintaro Matsumoto <matsushin35@gmail.com>
2021-05-08 14:17:31 +0900

    add to auth.js

--- app/auth.js
+++ app/auth.js
@@ -1,15 +1,29 @@
 const connection = require('./dbconnect')
-const bcypt = require('bcrypt')
+const bcypt = require('bcrypt') // パスワードハッシュ化
+
 
 // ユーザー認証
 // 引数 username: 文字列 ユーザー名
 // 引数 passwd: 文字列 パスワード
 // 戻り値 : 認証された場合 true 否認の場合 false
 exports.auth = (username, passwd) => {
-
-    if (hoge){
-        return true
-    } else{
-      return false
-    }
-}
+    const con = connection.createConnect();
+    con.connect();
+    con.query(
+        'SELECT * FROM users WHERE username = ?',[username],
+        (error, results) => {
+            // 入力されたユーザーネームのユーザーが存在するか
+            if (results.length > 0){
+                // 入力されたパスワードがDBと一致するか
+                if (passwd === results[0].password) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                // ユーザーネームが違う
+                return false;
+            }
+        }
+    );
+};
