e3e70614eb7c4a375dfe4620e528d27a90ea3c93
Shintaro Matsumoto <matsushin35@gmail.com>
2021-05-08 18:22:00 +0900

    cahange auth

--- app/auth.js
+++ app/auth.js
@@ -5,25 +5,31 @@ const bcypt = require('bcrypt') // パスワードハッシュ化
 // ユーザー認証
 // 引数 username: 文字列 ユーザー名
 // 引数 passwd: 文字列 パスワード
-// 戻り値 : 認証された場合 true 否認の場合 false
-exports.auth = (username, passwd) => {
+// 戻り値 bool: 認証された場合 true 否認の場合 false
+exports.authenticate = (user_id, passwd) => {
+    let is_auth = false
+    console.log(passwd)
     const con = connection.createConnect();
     con.connect();
     con.query(
-        'SELECT * FROM users WHERE username = ?',[username],
+        'SELECT * FROM users WHERE user_id = ?',[user_id],
         (error, results) => {
-            // 入力されたユーザーネームのユーザーが存在するか
+            console.log(results)
             if (results.length > 0){
-                // 入力されたパスワードがDBと一致するか
-                if (passwd === results[0].password) {
-                    return true;
+                console.log(results[0].password)
+                if (passwd == results[0].password) {
+                    return  true
                 } else {
-                    return false;
+                    return false
                 }
+                console.log(is_auth)
+                console.log(passwd === results[0].password)
+                return  true
             } else {
-                // ユーザーネームが違う
-                return false;
+                return false
             }
         }
-    );
-};
+    )
+    con.end()
+    return is_auth
+}
